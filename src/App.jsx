dfce6b65e45a86061cfd78dd10ed63f6830601b3
Jasser <Jasser@admin>
2024-01-28 21:22:55 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [currency, setCurrency] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://openapiv1.coinstats.app/coins', {
+          headers: {
+            'X-API-KEY': import.meta.env.VITE_X_API_KEY
+          }
+        });
+        setCurrency(response.data.result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // The empty dependency array ensures that the effect runs once on mount
+
+  return (
+    <>
+      <div>
+        <div className="App">
+          <h2>Crypto Currency Tracker</h2>
+          <input type='text' placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Market Cap</th>
+                <th>Price</th>
+                <th>Available Supply</th>
+                <th>Volume</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currency.filter((val) => val.name.toLowerCase().includes(search.toLowerCase())).map((val) => (
+                <tr key={val.rank}>
+                  <td className="rank">{val.rank}</td>
+                  <td className="logo">
+                    <a href={val.websiteUrl}>
+                      <img src={val.icon} alt="" />
+                    </a>
+                    <p>{val.name}</p>
+                  </td>
+                  <td className="symbol">{val.symbol}</td>
+                  <td>{val.marketCap}$</td>
+                  <td>{val.price.toFixed(2)}$</td>
+                  <td>{val.availableSupply}</td>
+                  <td>{val.volume.toFixed(2)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
